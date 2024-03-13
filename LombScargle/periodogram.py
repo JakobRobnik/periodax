BF seems to work now (fixed the prior and eliminated det in log-likelihood computation). It is kinda slow, probably set maxiter in optimization?

LombScargle/periodogram.py
@@ -98,12 +98,12 @@ def get_weight_func(sqrt_cov):
     elif len(sqrt_cov.shape) == 1:
         return lambda x: x / jnp.square(sqrt_cov)
     else:
-        return lambda x: jax.scipy.linalg.cho_solve((sqrt_cov, True), x) 
+        return lambda x: jax.scipy.linalg.cho_solve((sqrt_cov, True), x)
     
     
 def func(time, data, floating_mean= False, sqrt_cov= None):
     """sqrt_cov is the square root of a noise covariance matrix. It can be: 
-            None: periodogram will assum equal error, non-correlated noise
+            None: periodogram will assume equal error, non-correlated noise
             1d array: non-equal error, non-correlated noise. In this case sqrt_cov[i] is the error of data[i]
             2d array: correlated noise, this is the square root of the covariance matrix, meaning that it is L in its Cholesky decomposition Cov = L L^T. It can be obtained e.g. by L = jnp.linalg.cholesky(Cov)
     """
@@ -116,6 +116,7 @@ def func(time, data, floating_mean= False, sqrt_cov= None):
 
     else:
         return lambda freq: compute2(time, data, freq, weight_func)
+
 
 def log_prob_null(data, sqrt_cov):
     weight_func = get_weight_func(sqrt_cov)

LombScargle/psd.py
@@ -9,11 +9,11 @@ drw_kernel = lambda sigma, tau: lambda t1, t2: jnp.square(sigma) * jnp.exp(-jnp.
 def covariance(t, cov_func, errors):
     """Compute the covariance matrix S_ij = cov_func(t_i, t_j) + delta_ij err(t_i)"""
     t1, t2 = jnp.meshgrid(t, t)
-    return cov_func(t1, t2) + + jnp.diag(jnp.square(errors))
+    return cov_func(t1, t2) + jnp.diag(jnp.square(errors))
 
 
 
-def nlog_density(time, data, err, fmin, fmax):
+def nlog_density(time, data, err, fmin, fmax, floating_mean= True):
     
     
     def likelihood_alternative(y):
@@ -29,13 +29,11 @@ def nlog_density(time, data, err, fmin, fmax):
         
         # likelihood ratio (at maximal amplitudes) = log p(x|freq, params) / p(x|params)
         # note that this is not the maximum log-likelihood ratio, because params are not separately optimize under the null
-        periodogram_score = periodogram.func(time, data, floating_mean= False, sqrt_cov= sqrt_cov)(freq)[0]
-        log_det = jnp.sum(jnp.log(jnp.square(jnp.diag(sqrt_cov))))
-        logp_ratio = -0.5 * periodogram_score - 0.5 * log_det 
+        nlogp_ratio = 0.5* periodogram.func(time, data, floating_mean= floating_mean, sqrt_cov= sqrt_cov)(freq)[0]
         
         # eliminate p(x | params)
         logp_null = periodogram.log_prob_null(data, sqrt_cov)
-        return -logp_ratio - logp_null
+        return nlogp_ratio - logp_null
     
     def likelihood_null(y):
         """z = parameters of psd"""        
@@ -48,10 +46,10 @@ def nlog_density(time, data, err, fmin, fmax):
     def prior_null(y):
         log_mu = jnp.log(jnp.array([0.1, 120.]))
         sigma = jnp.array([0.2, 0.9])
-        return jnp.sum(0.5 * jnp.square((y[1:] - log_mu)/sigma) + 0.5 * jnp.log(2 * jnp.pi * jnp.square(sigma)))    
+        return jnp.sum(0.5 * jnp.square((y - log_mu)/sigma) + 0.5 * jnp.log(2 * jnp.pi * jnp.square(sigma)))    
         
     def prior_alternative(y):
-        return jnp.log(jnp.log(fmax/fmin)) + prior_null(y)
+        return prior_null(y[1:]) + jnp.log(jnp.log(fmax/fmin))
 
     def posterior_null(y):
         return prior_null(y) + likelihood_null(y)

hypothesis_testing/bayes_factor.py
@@ -42,7 +42,6 @@ def quadrature(nlogp, MAP, quad_scheme):
     """
     
     d = len(MAP.y)
-        
     ### go in the basis where the covariance matrix is the identity ###
     detCov = jnp.linalg.det(MAP.cov)
     D, Q = jnp.linalg.eigh(MAP.cov)
@@ -54,11 +53,10 @@ def quadrature(nlogp, MAP, quad_scheme):
         return jnp.exp(-nlogp(z) + MAP.nlogp + jnp.sum(jnp.square(X))) # residual integrand in this basis
     
     ### do the integral ###
-    val = quad.integrate(residual_integrand, *quad_scheme) 
+    val = quad.integrate(residual_integrand, *quad_scheme, d) 
     
     val *= jnp.sqrt(2**d * detCov) # for the change of basis
     logB = - MAP.nlogp + jnp.log(val)
-
     return logB
 
 
@@ -66,24 +64,27 @@ def quadrature(nlogp, MAP, quad_scheme):
 def logB(time, data, err_data, freq, floating_mean= False):
     """log Bayes factor for the sinusoidal variability in the correlated Gaussian noise (ignores the marginalization over the amplitude parameters)"""
     
-    null, alternative = psd.nlog_density(time, data, err_data, freq[0], freq[-1])
-    
+    null, alternative = psd.nlog_density(time, data, err_data, freq[0], freq[-1], floating_mean)
+
     ### analyze the null model ###
     y_init = jnp.log(jnp.array([0.1, 120])) # mode of the prior distribution
-    map = optimize(null, y_init)
-    log_ev0 = quadrature(null, map, e2r2)
+    map0 = optimize(null, y_init)
+    #print('null params:', jnp.exp(map0.y))
+    log_ev0 = quadrature(null, map0, e2r2)
     
     ### analyze the alternative model ###
     # find the best candidate for the alternative
-    cov = psd.covariance(time, psd.drw_kernel(*jnp.exp(map.y)), err_data)
+    cov = psd.covariance(time, psd.drw_kernel(*jnp.exp(map0.y)), err_data)
     score, _ = jax.vmap(periodogram.func(time, data, floating_mean, jnp.linalg.cholesky(cov)))(freq)
     freq_best = freq[jnp.argmax(score)]
+    print('best period: ', 1/freq_best, 'score: ', jnp.max(score))
     
     # compute the evidence
-    y_init = jnp.array([jnp.log(freq_best), *map.y])
-    map = optimize(alternative, y_init)
-    log_ev1 = quadrature(alternative, map, e3r2)
-    
+    y_init = jnp.array([jnp.log(freq_best), *map0.y])
+    map1 = optimize(alternative, y_init)
+    #print('best period ', jnp.exp(-map1.y[0]), 'psd params: ', jnp.exp(map1.y[1:]))
+    log_ev1 = quadrature(alternative, map1, e3r2)
+    print(log_ev1, log_ev0)
     
     ### return the log Bayes factor and the optimal parameters ###
-    return log_ev1 - log_ev0, jnp.exp(map.y)
+    return log_ev1 - log_ev0, jnp.exp(map1.y)

hypothesis_testing/quad.py
@@ -7,14 +7,14 @@ import jax
 import jax.numpy as jnp
 
 
-def integrate(f, x, w):
+def integrate(f, x, w, d):
     """approximates the integral I = int e^{-x^2} f(x) dx for a three dimensional x by a quadrature:
         I = sum_k f(x_k) w_k
         x: points where the f is evaluated
         w: weights for those points
     """
     
-    return jnp.sum(w * jax.vmap(f)(x)) * jnp.power(jnp.pi, 1.5) # this factor is for the integral e^{-x^2} dx = pi^3/2
+    return jnp.sum(w * jax.vmap(f)(x)) * jnp.power(jnp.pi, d * 0.5) # this factor is for the integral e^{-x^2} dx = pi^d/2
 
 
 

simulations/util.py
@@ -22,7 +22,7 @@ def regular_spaced(key, PSD, mask):
 
 def irregular_spaced(key, cov):
     
-    return jnp.linalg.cholesky(cov) @ jax.random.normal(key, (cov.shape[0], ))
+    return jnp.linalg.cholesky(cov) @ jax.random.normal(key, shape= (cov.shape[0], ))
 
 
 