81e373081e8bc1cc56ad21e6a162615990dda3c9
jakob.robnik@gmail.com <jakob.robnik@gmail.com>
2024-04-11 01:04:32 -0700

    saving polynomial fit

--- quasars/prep.py
+++ quasars/prep.py
@@ -1,7 +1,8 @@
 import jax
 import jax.numpy as jnp
 import pandas as pd
-
+from scipy.signal import medfilt
+import numpy as np
 
 scratch_base = '/pscratch/sd/j/jrobnik/'
 scratch = scratch_base + 'quasars_scratch/'
@@ -12,7 +13,7 @@ ids = jnp.load(dir_data + 'ids.npy')
 #ids = jnp.array(pd.read_csv('quasars/known.csv')['id'], dtype= int)
 
 
-def average_within_night(time, data, err):
+def within_night_averaging(time, data, err):
     """average the data within the same night"""
     
     midnight = 8./24. # specific to the time zone
@@ -29,9 +30,32 @@ def average_within_night(time, data, err):
     
     has_data = invVar != 0. # eliminate nights where there are no measurements
     return Time[has_data] / invVar[has_data], Data[has_data] / invVar[has_data], jnp.sqrt(1./invVar[has_data]) # normalize the sums and return results
+
+
+
+def median_3point(_x):
+
+    # zero paddle the data
+    what_insert = jnp.array([0, -1])
+    where_insert = jnp.array([0, -1])
+    x = jnp.insert(_x, where_insert, _x[what_insert])
+    
+    X = jnp.array([x[:-2], x[1:-1], x[2:]]) # array of shape (3, len(_x)), created by shifting _x
+    return jnp.median(X, axis = 0) # take a median along the first axis
+
+
+def outlier_removal(time, data):
+    y = median_3point(data) # three point median filter
+    p = jnp.polyfit(time, y, deg= 5) # fit the polynomial coefficients
+    polynominal = jnp.polyval(p, time) # compute the polynomial
+    residuals = data-polynominal
+    sigma = jnp.std(residuals) # compute sigma
+    mask = jnp.abs(residuals) > 3 * sigma
+    return mask
     
 
-def prepare_data(id, night_average= True):
+
+def prepare_data(id, remove_outliers= True, average_within_night= True):
     
     # load the data
     df = pd.read_csv(dir_data + str(id) + '.csv')
@@ -39,9 +63,16 @@ def prepare_data(id, night_average= True):
     mag = jnp.array(df['mag'])
     mag_err = jnp.array(df['mag_err'])
     
-    if night_average:
-        time, mag, mag_err = average_within_night(time, mag, mag_err)
+    # sort the data
+    perm = jnp.argsort(time)
+    time, mag, mag_err = time[perm], mag[perm], mag_err[perm]
     
+    if remove_outliers:
+        mask = outlier_removal(time, mag)
+        time, mag, mag_err = time[mask], mag[mask], mag_err[mask]
+    
+    if average_within_night:
+        time, mag, mag_err = within_night_averaging(time, mag, mag_err)
     
     # determine the frequency grid
     T = jnp.max(time) - jnp.min(time)
