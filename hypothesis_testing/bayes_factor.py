2c735318fc416bbaf9e78f20335b21ab8aab6706
Jakob Robnik <43053552+JakobRobnik@users.noreply.github.com>
2024-11-27 10:55:12 -0800

    Update bayes_factor.py

--- hypothesis_testing/bayes_factor.py
+++ hypothesis_testing/bayes_factor.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from typing import NamedTuple, Any
 
 from . import quad
-from ..LombScargle import periodogram, psd
+from ..LombScargle import periodogram, drw
 
 scheme_d2 = quad.get_scheme(2, 6)
 scheme_d3 = quad.get_scheme(3, 7)
